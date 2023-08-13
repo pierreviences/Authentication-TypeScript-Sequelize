4619a16c4421dbc06beeb688fe21a5a40a8829dc
pierreviences <imaniyazid@gmail.com>
2023-08-13 13:06:18 +0700

    feat: Add logic CRUD Role Function

--- src/controller/RoleConstroller.ts
+++ src/controller/RoleConstroller.ts
@@ -68,4 +68,115 @@ const createRole = async (req: express.Request, res: express.Response) => {
   }
 };
 
+const updateRole = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const { roleName, active } = req.body;
+    const role = await Role.findByPk(id);
+    if (!role) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data not found",
+        data: null,
+      });
+    }
+
+    role.roleName = roleName;
+    role.active = active;
+    await role.save();
+
+    return res.status(200).json({
+      status: 200,
+      message: "OK",
+      data: role,
+    });
+  } catch (error: any) {
+    if (error != null && error instanceof Error) {
+      return res.status(500).send({
+        status: 500,
+        message: error.message,
+        errores: error,
+      });
+    }
+
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Eror",
+      errors: error,
+    });
+  }
+};
+
+const deleteRole = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const { roleName, active } = req.body;
+    const role = await Role.findByPk(id);
+    if (!role) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data not found",
+        data: null,
+      });
+    }
+
+    await role.destroy();
+
+    return res.status(200).json({
+      status: 200,
+      message: "Deleted",
+      data: null,
+    });
+  } catch (error: any) {
+    if (error != null && error instanceof Error) {
+      return res.status(500).send({
+        status: 500,
+        message: error.message,
+        errores: error,
+      });
+    }
+
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Eror",
+      errors: error,
+    });
+  }
+};
+
+const GetRoleById = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const { roleName, active } = req.body;
+    const role = await Role.findByPk(id);
+    if (!role) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data not found",
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      status: 200,
+      message: "Data berhasil ditemukan",
+      data: role,
+    });
+  } catch (error: any) {
+    if (error != null && error instanceof Error) {
+      return res.status(500).send({
+        status: 500,
+        message: error.message,
+        errores: error,
+      });
+    }
+
+    return res.status(500).json({
+      status: 500,
+      message: "Internal Server Eror",
+      errors: error,
+    });
+  }
+};
+
 export default { getRole, createRole, updateRole, deleteRole, GetRoleById };
