067f53679595d1975b4cf4297aca8b6afbed0b6b
pierreviences <imaniyazid@gmail.com>
2023-08-14 14:08:34 +0700

    feat: Add Validator with validatorjs

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import RoleConstroller from "../controller/RoleConstroller";
 import UserController from "../controller/UserController";
+import UserValidation from "../middleware/validation/UserValidation";
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ router.delete("/role/:id", RoleConstroller.deleteRole);
 router.get("/role/:id", RoleConstroller.GetRoleById);
 
 // User Routing
-router.post("/user/register", UserController.Register);
+router.post(
+  "/user/register",
+  UserValidation.RegisterValidation,
+  UserController.Register
+);
 
 export default router;
