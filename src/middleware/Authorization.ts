d397b201b3414c94335cec66786629961414467c
pierreviences <imaniyazid@gmail.com>
2023-08-14 20:41:26 +0700

    feat:  User Login (Authorization & Authentication

---
+++ src/middleware/Authorization.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import Helper from "../helpers/Helper";
+import Jwt from "../helpers/Jwt";
+
+const Authenticated = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authToken = req.headers["authorization"];
+    const token = authToken && authToken.split(" ")[1];
+    if (token === null) {
+      return res
+        .status(401)
+        .send(Helper.ResponseData(401, "Unathorized", null, null));
+    }
+    const result = Jwt.ExtractToken(token!);
+    next();
+  } catch (error: any) {
+    res.status(500).send(Helper.ResponseData(500, "", error, null));
+  }
+};
+
+export default { Authenticated };
