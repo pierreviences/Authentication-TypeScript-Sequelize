9c574ed8971c31aa7e1e003c5d397d0ccc04b13b
pierreviences <imaniyazid@gmail.com>
2023-08-14 20:41:11 +0700

    feat: Add JsonWebtoken generated

---
+++ src/helpers/Jwt.ts
@@ -0,0 +1,68 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+interface UserData {
+  name: string | null;
+  email: string | null;
+  roleId: string | null;
+  verified: boolean | null;
+  active: boolean | null;
+}
+
+const GenerateToken = (data: any): string => {
+  const token = jwt.sign(data, process.env.JWT_TOKEN as string, {
+    expiresIn: "10m",
+  });
+  return token;
+};
+
+const GenerateRefreshToken = (data: any): string => {
+  const token = jwt.sign(data, process.env.JWT_REFRESH_TOKEN as string, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+const ExtractToken = (token: string) => {
+  const secretKey = process.env.JWT_TOKEN as string;
+  let resData: any;
+  const res = jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      resData = null;
+    } else {
+      resData = decoded;
+    }
+
+    if (resData) {
+      const result: UserData = <UserData>resData;
+      return result;
+    }
+    return null;
+  });
+};
+
+const ExtractRefreshToken = (token: string) => {
+  const secretKey = process.env.JWT_REFRESH_TOKEN as string;
+  let resData: any;
+  const res = jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      resData = null;
+    } else {
+      resData = decoded;
+    }
+
+    if (resData) {
+      const result: UserData = <UserData>resData;
+      return result;
+    }
+    return null;
+  });
+};
+
+export default {
+  GenerateToken,
+  GenerateRefreshToken,
+  ExtractRefreshToken,
+  ExtractToken,
+};
